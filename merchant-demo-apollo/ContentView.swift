2a9462b4bb1cd3042dcc16780bc09edf0fc222c8
Enes Ozturk <enesozturk.d@gmail.com>
2021-05-20 11:58:53 +0300

    Initial Commit

---
+++ merchant-demo-apollo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  merchant-demo-apollo
+//
+//  Created by Enes Öztürk on 20.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ merchant-demo-apollo/merchant_demo_apolloApp.swift
@@ -0,0 +1,17 @@
+//
+//  merchant_demo_apolloApp.swift
+//  merchant-demo-apollo
+//
+//  Created by Enes Öztürk on 20.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct merchant_demo_apolloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
